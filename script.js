1ecc0171e27ec2b7d0911d4fa1dd52cf5133be05
Navkaran Singh Sidhu <nsidhu10@academic.rrc.ca>
2025-02-13 01:05:45 -0600

    feat: addded buttons and transitions.

---
+++ script.js
@@ -0,0 +1,42 @@
+let currentState = 10;
+
+const renderQuestion = () => {
+    const paragraphNode = document.querySelector("#question-text");
+    const buttonContainer = document.querySelector("#answers");
+    buttonContainer.innerHTML = ""; 
+    switch(currentState) {
+        case 10: 
+            paragraphNode.textContent = "You wake up in a dark forest.\
+             Two paths lie ahead. Which one do you take?";
+            renderButton("Take the left path", 10.1);
+            renderButton("Take the right path", 10.2);
+            break;
+        case 10.1:
+            paragraphNode.textContent = "You find a cave with \
+            glowing lights inside. What do you do?";
+            renderButton("Enter the cave", 11);
+            renderButton("Turn back", 10);
+            break;
+        case 10.2:
+            paragraphNode.textContent = "You see an abandoned cabin.\
+             There's a strange sound inside. What do you do?";
+            renderButton("Open the door"); 
+            renderButton("Run away", 10);
+            break; 
+            default:
+            console.log("You Won");
+    }
+};
+// create a button
+const renderButton = (addAnswerButton, transitionButton) => {
+    const newButtonNode = document.createElement("button");
+    newButtonNode.textContent = addAnswerButton;
+    newButtonNode.addEventListener("click", () => {
+        currentState = transitionButton;
+        console.log(currentState);
+        renderQuestion();
+    });
+    // document.getElementBy
+    document.querySelector("#answers").appendChild(newButtonNode);
+}
+renderQuestion();
